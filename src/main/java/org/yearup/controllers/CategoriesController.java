545c04187644da831d8ca1ad06414290df1bbb15
JustinK <productivekeys@gmail.com>
2026-06-21 17:20:01 -0500

    add restconroller boilerplate annotations

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,9 +1,8 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
@@ -12,22 +11,32 @@ import org.yearup.service.ProductService;
 import java.util.List;
 
 // add the annotations to make this a REST controller
+@RestController
 // add the annotation to make this controller the endpoint for the following url
+@RequestMapping("categories")
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@CrossOrigin
 public class CategoriesController
 {
-    private CategoryService categoryService;
-    private ProductService productService;
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+
 
 
     // create an Autowired constructor to inject the categoryService and productService
-
+    @Autowired
+    public CategoriesController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+    @GetMapping
     // add the appropriate annotation for a get action
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryService.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
